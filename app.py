6795ccf8423fbe2622b9fc6dd8010a9d8eba9eae
Mimoune <djouallah@users.noreply.github.com>
2021-04-15 06:37:23 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import json
+import streamlit as st
 from google.cloud import bigquery
+from google.oauth2 import service_account
 
-# Construct a BigQuery client object.
-client = bigquery.Client()
+# Retrieve and convert key file content.
+bigquery_key_json = json.loads(st.secrets["bigquery_key"], strict=False)
+credentials = service_account.Credentials.from_service_account_info(bigquery_key_json)
+
+# Create API client.
+client = bigquery.Client(credentials=credentials)
+
 
 query = """
     SELECT * FROM test-187010.ReportingDataset.today_Table  where now =1
