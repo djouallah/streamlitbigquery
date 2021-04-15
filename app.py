63bd2d0e94c9781d47f4223e4439582adf39991a
Mimoune <djouallah@users.noreply.github.com>
2021-04-15 13:15:04 +1000

    Update app.py

--- app.py
+++ app.py
@@ -32,3 +32,33 @@ c = alt.Chart(result2).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis
                                             ).add_selection(selection)
 st.write(c)
 st.write(result)
+
+#download
+def download_link(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+
+# Examples
+if st.button('Download Dataframe as CSV'):
+    tmp_download_link = download_link(result, 'YOUR_DF.csv', 'Click here to download your data!')
+    st.markdown(tmp_download_link, unsafe_allow_html=True)
+
+
