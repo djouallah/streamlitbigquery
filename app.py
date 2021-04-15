2c1f0b0bccc79e9eec1e92067e72d8ef881ec85a
Mimoune <djouallah@users.noreply.github.com>
2021-04-15 11:55:38 +1000

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,17 @@ from google.oauth2 import service_account
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
-
-
 for i in range(1, 101):
     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
     status_text.text("%i%% Complete" % i)
-    
     progress_bar.progress(i)
     last_rows = new_rows
     time.sleep(0.05)
-
 progress_bar.empty()
-
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
 # rerun.
-st.button("Re-run")
+st.button("Refresh")
 
 # Retrieve and convert key file content.
 bigquery_key_json = json.loads(st.secrets["bigquery_key"], strict=False)
