a0d33dc21f21f4eef618c0508b8086a312135ead
Mimoune <djouallah@users.noreply.github.com>
2021-04-15 15:04:30 +1000

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ c = alt.Chart(result2).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis
                                             height=300
                                             ).add_selection(selection)
 st.write(c)
-result2=result.groupby(['hourminute'])['RRP'].min().reset_index()
+result2=result.groupby(['hourminute'])['RRP'].average().reset_index()
 
 c = alt.Chart(result2).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis(labels=False)),
                                                     y='RRP',
