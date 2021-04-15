967a33f6d5d8913f2e50a1bfca2920fbfcdd9fa2
Mimoune <djouallah@users.noreply.github.com>
2021-04-15 11:29:11 +1000

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ bigquery_key_json = json.loads(st.secrets["bigquery_key"], strict=False)
 credentials = service_account.Credentials.from_service_account_info(bigquery_key_json)
 # Create API client.
 client = bigquery.Client(credentials=credentials)
-query = "SELECT SETTLEMENTDATE, Region, sum(SCADAVALUE) as Mw FROM `test-187010.ReportingDataset.today_Table`  group by 1,2"
+query = "SELECT hourminute, Region, sum(SCADAVALUE) as Mw FROM `test-187010.ReportingDataset.today_Table`  group by 1,2"
 result = pd.read_gbq(query, credentials=credentials)
 # now have a DF result do stuff with it
-column = result["SETTLEMENTDATE"]
+column = result["hourminute"]
 now = column.max()
 st.text("Nem Power Generation as of")
 st.write(now)
-c = alt.Chart(result).mark_area().encode(  x='SETTLEMENTDATE', y='Mw',color='Region', tooltip=['SETTLEMENTDATE','Region', 'Mw'])
+c = alt.Chart(result).mark_area().encode(  x='hourminute', y='Mw',color='Region', tooltip=['hourminute','Region', 'Mw'])
 st.write(c)
 st.write(result)
