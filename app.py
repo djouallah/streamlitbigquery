0431e00c485ab8b56489f7b5c6be8cc1d81a058d
Mimoune <djouallah@users.noreply.github.com>
2021-04-15 11:46:26 +1000

    Update app.py

--- app.py
+++ app.py
@@ -9,20 +9,22 @@ bigquery_key_json = json.loads(st.secrets["bigquery_key"], strict=False)
 credentials = service_account.Credentials.from_service_account_info(bigquery_key_json)
 # Create API client.
 client = bigquery.Client(credentials=credentials)
-query = "SELECT hourminute, Region, sum(SCADAVALUE) as Mw FROM `test-187010.ReportingDataset.today_Table`  group by 1,2"
+query = "SELECT hourminute, Technology, sum(SCADAVALUE) as Mw FROM `test-187010.ReportingDataset.today_Table`  group by 1,2"
 result = pd.read_gbq(query, credentials=credentials)
 # now have a DF result do stuff with it
 column = result["hourminute"]
 now = column.max()
 st.text("Nem Power Generation as of")
 st.write(now)
-selection = alt.selection_multi(fields=['Region'], bind='legend')
+selection = alt.selection_multi(fields=['Technology'], bind='legend')
 c = alt.Chart(result).mark_area().encode(  x=alt.X('hourminute',axis=alt.Axis(labels=False)),
-                                                    y='Mw',color='Region',
-                                                    tooltip=['hourminute','Region', 'Mw'],
-                                                    opacity=alt.condition(selection, alt.value(1), alt.value(0.2))).properties(
-    width=800,
-    height=300
-).add_selection(selection)
+                                                    y='Mw',
+                                                    color='Technology',
+                                                    tooltip=['hourminute','Technology', 'Mw'],
+                                                    opacity=alt.condition(selection, alt.value(1), alt.value(0.2))
+                                        ).properties(
+                                            width=800,
+                                            height=300
+                                            ).add_selection(selection)
 st.write(c)
 st.write(result)
