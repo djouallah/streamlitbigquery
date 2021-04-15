e7290d6a2ede8c7c5e587059196eee8174525e8f
Mimoune <djouallah@users.noreply.github.com>
2021-04-15 14:12:02 +1000

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,6 @@ def download_link(object_to_download, download_filename, download_link_text):
 
 # Examples
 tmp_download_link = download_link(result, 'YOUR_DF.csv', 'Click here to download the data for today')
-st.markdown(tmp_download_link, unsafe_allow_html=True)
+col2.markdown(tmp_download_link, unsafe_allow_html=True)
 
 
