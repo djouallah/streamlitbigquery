53b7b259d520f27c8b235b65a20bde0e534f202e
Mimoune <djouallah@users.noreply.github.com>
2022-07-17 20:03:43 +1000

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,8 @@ col1, col2 = st.columns([3, 1])
 
 with st.expander("See explanation"):
      st.write("""
-         The chart above shows some numbers I picked for you.
-         I rolled actual dice for these, so they're *guaranteed* to
-         be random.
+         Data is loaded from External Data Source and Cached as a parquet file, it is refreshed every 5 minutes, the local Interaction
+         use DuckDB for calculation, it is faster as there is no network latency and cost less as the Source system is not used
      """)
 
 ################################################### Download Data from BigQuery#####################################################
