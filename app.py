4270b501c9092be2974b1d6e5f955586e0ec32a9
Mimoune <djouallah@users.noreply.github.com>
2022-09-14 19:23:28 +1000

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ credentials = service_account.Credentials.from_service_account_info(bigquery_key
 # Create API client.
 client = bigquery.Client(credentials=credentials)
 query2 = '''--Streamlit 
-        SELECT hourminute,StationName,Region, Technology, sum(SCADAVALUE) as Mw,min(RRP) as RRP
-        FROM `test-187010.ReportingDataset.today_Table`  group by 1,2,3,4'''
+        SELECT hourminute,StationName,Region, Technology,SETTLEMENTDATE, sum(SCADAVALUE) as Mw,min(RRP) as RRP
+        FROM `test-187010.ReportingDataset.today_Table` where rrp is not null and SCADAVALUE != 0 group by 1,2,3,4,5'''
 
 # Query BgQuery
 p = Path("data.parquet")
@@ -37,33 +37,27 @@ if not p.exists():
 ###################################################################################################################################
 
 con = duckdb.connect()
-StationName =con.execute('''select distinct StationName from read_parquet('data.parquet') order by StationName ;''').fetchdf()
+con.execute('''create or replace view source as select * from read_parquet('data.parquet')''')
+StationName =con.execute('''select distinct StationName from source order by StationName ;''').fetchdf()
 option = st.multiselect('Select Stations',StationName['StationName'])
    
 xxxx = "','".join(option)
         
 select = (f'''('{xxxx}')''')
 if len(option) == 0 : 
-     Query = '''select * from read_parquet('data.parquet')  '''
+     result =con.execute('''select * from source ''').df()
 else :
-     Query = '''select * from read_parquet('data.parquet') where  StationName in '''+str(select)
+     result =con.execute('select * from source where  StationName in '+str(select)).df()
         #st.write(Query)
-con = duckdb.connect()
-result =con.execute(Query).fetchdf()
-con.close()
-now = result["hourminute"].max()
-        #st.dataframe(result)
+now = con.execute('''select strftime(max(SETTLEMENTDATE), '%A, %-d %B %Y - %I:%M:%S %p') as SETTLEMENTDATE  from result''').df()
+now = now[['SETTLEMENTDATE']].values[0][0]
 
-st.subheader("Nem Power Generation Today: " + str(now))
-        #st.write(now)
-result2=result.groupby(['hourminute','StationName','Region'])['Mw'].sum().reset_index()
+st.subheader( str(now))
+result1=  con.execute('select hourminute,StationName,Region, sum (Mw) as Mw from result group by all').df()
 selection = alt.selection_multi(fields=['StationName'], bind='legend')
-c = alt.Chart(result2).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis(labels=False,ticks=False)),
+c = alt.Chart(result1).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis(labels=False,ticks=False)),
                                                             y='sum(Mw):Q',
                                                             color=alt.Color('StationName'),
-                                                            
-                                                            
-                                                            
                                                             tooltip=['hourminute','StationName', 'Mw'],
                                                             
                                                 ).properties(
@@ -74,8 +68,8 @@ c = alt.Chart(result2).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis
                                                     columns=5
                                                                     ).resolve_scale(y='independent')
 
-xxx=result.groupby(['hourminute','Region'])['Mw'].sum().reset_index()
-c2 = alt.Chart(xxx).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis(labels=False,ticks=False)),
+result2=con.execute('select hourminute,Region, sum (Mw) as Mw from result group by all').df()
+c2 = alt.Chart(result2).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis(labels=False,ticks=False)),
                                                             y='sum(Mw):Q',
                                                             color=alt.Color('Region'),
                                                             tooltip=['hourminute','Region', 'Mw'],
@@ -89,10 +83,7 @@ c2 = alt.Chart(xxx).mark_area().encode(  x=alt.X('hourminute:O',axis=alt.Axis(la
                                                                     ).resolve_scale(y='independent')
 
 
-
-
-result3=result.groupby(['hourminute','Region'])['RRP'].mean().reset_index()
-
+result3=con.execute('select hourminute,Region, Min (RRP) as RRP from result group by all').df()
 c1 = alt.Chart(result3).mark_bar().encode(  x=alt.X('hourminute:O',axis=alt.Axis(labels=False,ticks=False)),
                                                             y='RRP',
                                                             color=alt.condition(
@@ -116,7 +107,7 @@ else :
     st.write(c1)
     st.write(c2)
     
-
+del result1,result2,result3
 #Download Button
 
 
@@ -134,14 +125,10 @@ col2.download_button(
 
 link='[Blog](https://datamonkeysite.com/category/streamlit/)'
 col1.markdown(link,unsafe_allow_html=True)
-
-
 ################## refresh data every 5 minutes 
 st_autorefresh(interval=4 * 60 * 1000, key="dataframerefresh")
 @st.experimental_memo(ttl=300)
 def Get_Bq(query,_cred) :
         df=pd.read_gbq(query,credentials=_cred)
         df.to_parquet('data.parquet')
-
-
 Get_Bq(query2,credentials)
